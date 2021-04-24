eb669a3f8e50323c0f797902613acc5872888095
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-04-25 02:03:17 +0600

    Create 560A.cpp

---
+++ 560A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=1,m;
+    cin>>m;
+    for(int i=0; i<m; i++)
+    {
+        int a;
+        cin>>a;
+        if(a==1)
+        {
+            n=-1;
+            break;
+        }
+
+    }
+    cout<<n<<endl;
+    return 0;
+}
