980192f8ab7ff5768dbe86210cb99950ed137370
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-23 10:24:44 +0600

    Create 4A.cpp

---
+++ 4A.cpp
@@ -0,0 +1,21 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	if(n>2 && n%2==0)
+	{
+		cout<<"YES\n";
+	}
+	else
+	{
+		cout<<"NO\n";
+	}
+}
