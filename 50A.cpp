904ba7484d2a3097969ce7163f1753730ceed7ee
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-28 01:04:41 +0600

    Create 50A.cpp

---
+++ 50A.cpp
@@ -0,0 +1,14 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int m,n;
+	cin>>m>>n;
+	cout<<(m*n)/2<<endl;
+}
