cfc7ad5a9850f8f50f8b08303400dd319d405f22
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-04 08:57:33 +0600

    Create 281A.cpp

---
+++ 281A.cpp
@@ -0,0 +1,15 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    s[0]=toupper(s[0]);
+    cout<<s<<endl;
+}
