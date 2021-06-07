329e7ce878c99a89968c401f920d9e8d96fa3464
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-07 14:15:54 +0600

    Update 149A.cpp

--- 149A.cpp
+++ 149A.cpp
@@ -19,28 +19,10 @@ typedef unsigned long long ull;
 
 int main()
 {
-    int k,a[12],total(0);
-    cin>>k;
-    for(size_t i=0; i<12; ++i)
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++)
     {
-        cin>>a[i];
-        total+=a[i];
-    }
-    if(total<k)
-    {
-        cout<<-1<<endl;
-    }
-    else
-    {
-        sort(a,a+sizeof(a)/sizeof(a[0]), greater <int>());
-        int sum=0;
-        int month=0;
-        while(sum<k)
-        {
-            sum+=a[month];
-            month+=1;
-        }
-        cout<<month<<endl;
+        cout<<i<<" "<<i*i<<" "<<i*i*i<<endl;
     }
 }
-
