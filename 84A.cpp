960044273fd7646e76bcf4e24349868a6e94f8e6
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-08 10:03:04 +0600

    Create 84A.cpp

---
+++ 84A.cpp
@@ -0,0 +1,26 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long l;
+typedef vector<int> vint;
+typedef pair<int,int> pint;
+typedef unsigned long long ull;
+
+#define sc scanf
+#define pf printf
+#define nl pf("\n")
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<(n/2)*3<<endl;
+}
+
