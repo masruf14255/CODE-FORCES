9050a98ebe000e0025c5eb06f6123b22a5bd0e66
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-07-27 18:36:14 +0600

    Create Number Theory - Beginner (BACS-BUBT National Programming Camp, 2017).cpp

---
+++ Number Theory - Beginner (BACS-BUBT National Programming Camp, 2017).cpp
@@ -0,0 +1,11 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+
+
+prime number--
+
+
